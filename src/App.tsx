44d4a93dd200f1c6849c3f7d015f11a52e70307b
Serhii Rud <carmagsales@gmail.com>
2023-12-31 16:35:09 +0200

    clear project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
-export const App = () => {
-  return <h1 className="fon-bold text-3xl text-red-500">Burger Mayster</h1>;
-};
+import React from "react";
+
+function App() {
+  return <div className="App">test</div>;
+}
+
+export default App;
