473a29c64df878fcb0f9176fbb47264e4c57e425
Serhii Rud <carmagsales@gmail.com>
2024-01-02 20:01:20 +0200

    header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
-import { chakra, Box } from "@chakra-ui/react";
+import { chakra, Box, Image, Text } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
 
 export const Header = () => {
   return (
-    <chakra.header bg="red.100" w="1480px">
-      <Container maxW="1440px" bg="blue.100">
-        Header
+    <chakra.header border="1px dotted red" w="1920" p="10px 30px 10px 30px">
+      <Container maxW="1920px" bg="white" m="0 auto">
+        <chakra.a href="/" display="flex" gap="5" alignItems="center">
+          <Image
+            src="../../images/png/hamburger-85x64.png"
+            alt="hamburger"
+          ></Image>
+          <Text fontFamily="Satisfy" fontSize={32} color="#7b3d22">
+            Burger Mayster
+          </Text>
+        </chakra.a>
       </Container>
     </chakra.header>
   );
