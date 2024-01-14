dcd2f517cbfa9704e79d3f19b9523f3c4fab9934
Serhii Rud <carmagsales@gmail.com>
2024-01-14 16:34:21 +0200

    home page bcg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,11 +2,16 @@ import { Container, Heading, Text } from "@chakra-ui/react";
 
 export const Home = () => {
   return (
-    <Container h="1200px">
-      <Heading as="h1" mt="20px" mb="20px" textAlign="center" fontSize="24">
+    <Container
+      h="1200px"
+      maxW="1200px"
+      backgroundImage="linear-gradient(to-t, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.9)), url(../../images/png/home-background.jpg)"
+      backgroundRepeat="no-repeat"
+    >
+      <Heading as="h1" mt="20px" mb="40px" textAlign="center" fontSize="24">
         Home page
       </Heading>
-      <Text m="0 auto" w="1200px">
+      <Text>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, autem
         harum eligendi explicabo enim impedit provident dolorem repellat fugiat
         maiores tenetur ipsam? Consectetur voluptates provident praesentium
